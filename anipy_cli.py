40a4ca1318fa317e04b9a2e6f1b6f90bf9e2fabe
Denis Strizhkin <strdenis02@gmail.com>
2023-02-17 19:22:03 +0300

    Add main file

---
+++ anipy_cli.py
@@ -0,0 +1,15 @@
+#!/bin/python
+# from anipy_cli import cli
+# from anipy_cli.misc import keyboard_inter
+
+def main():
+    try:
+        # cli.main()
+        pass
+    except KeyboardInterrupt:
+        # keyboard_iter()
+        pass
+
+
+if __name__ == "__main__":
+    main()
